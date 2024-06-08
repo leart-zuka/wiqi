9c54c4d4ec125ef8de51f44769bf1055fb38c9df
Leart Zuka <leart.zuka@web.de>
2024-06-08 04:43:52 +0200

    chore: styling...

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -10,26 +10,47 @@ export default function Home() {
     const [locale, setLocale] = useState(useLocale())
     const [difficulty, setDifficulty] = useState("Elementary School Student")
     return (
-        <main >
-            <nav className="fixed bg-white border-gray-200 top-5 w-screen start-0 flex flex-wrap items-center mx-auto h-auto justify-between">
-                <DropDown className={"space-x-10 w-20 scale-75 rtl:space-x-3"} stateChange={setDifficulty} />
-                <Link href={locale === 'de' ? '/en' : '/de'}>
-                    <Image
-                        src={useLocale() + ".svg"}
-                        alt={useLocale() + " Flag"}
-                        width={50}
-                        height={12}
-                        className="self-center p-2 md:order-2"
-                        priority
-                    />
-                </Link>
+        <main>
+            <nav className="bg-opacity-20 bg-white w-full start-0 h-fit">
+                <div className="flex flex-wrap items-center mx-auto justify-between p-2">
+                    <a href="https://www.pushquantum.tech/" className="flex items-center space-x-0">
+                        <img src="pq_logo.svg" className="h-8 p-2" alt="PushQuantum Logo" />
+                        <span className="text-lg font-semibold italic dark:text-white"> PushQuantum</span>
+                    </a>
+                    <DropDown className={"relative right-12 w-20"} stateChange={setDifficulty} />
+                    <Link href={locale === 'de' ? '/en' : '/de'}>
+                        <Image
+                            src={useLocale() + ".svg"}
+                            alt={useLocale() + " Flag"}
+                            width={50}
+                            height={12}
+                            className="self-center p-2 md:order-2"
+                            priority
+                        />
+                    </Link>
+                </div>
             </nav>
-
-            <div className="relative top-52">
-                <p className="text-center">
+            <div className="text-center top-24 h-fit">
+                <p className="">
                     {t(`${difficulty}`)}
                 </p>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
+                <h1> Hi this is a text </h1>
             </div>
         </main >
     );
+}
+
+const main_colors = {
+    dark_blue: "#06014a",
+    pink: "#fe2a77"
 }
