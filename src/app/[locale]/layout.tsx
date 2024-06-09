512d631d2fdd97d04c1078dab50a90cdef75bb9a
Leart Zuka <leart.zuka@web.de>
2024-06-10 01:37:58 +0200

    fix: latex rendering error

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../globals.css";
+import "katex/dist/katex.min.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 
