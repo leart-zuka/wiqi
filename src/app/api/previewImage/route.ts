e8c30f39802fb7ad69cd7cdfba12a3b6f1091d13
Leart Zuka <leart.zuka@web.de>
2024-06-17 14:35:14 +0200

    try: enable image return

--- src/app/api/previewImage/route.ts
+++ src/app/api/previewImage/route.ts
@@ -6,7 +6,6 @@ export async function POST(req: NextRequest, res: NextResponse) {
         const body = await req.json();
         console.debug(body)
         const image = await getImageBase64(body.url);
-        return NextResponse.json({ "test": "hi" }, { status: 204 })
         return NextResponse.json({ image }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ "error": "Couldn't fetch screenshot off of website 2" }, { status: 500 })
