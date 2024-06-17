6eff16f16c51c833e89972b48bdc5cb2110557bd
Leart Zuka <leart.zuka@web.de>
2024-06-17 14:02:53 +0200

    try: new responses

--- src/app/api/previewImage/route.ts
+++ src/app/api/previewImage/route.ts
@@ -1,12 +1,13 @@
 import puppeteer from "puppeteer";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: NextRequest, res: NextResponse) {
     try {
         const body = await req.json();
         console.debug(body)
         const image = await getImageBase64(body.url);
-        return Response.json({ image });
+        return NextResponse.json({ "test": "hi" }, { status: 204 })
+        return NextResponse.json({ image }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ "error": "Couldn't fetch screenshot off of website 2" }, { status: 500 })
     }
@@ -19,6 +20,7 @@ let getImageBase64 = async (url: string) => {
         await page.goto(url);
         let image = await page.screenshot({ encoding: "base64" });
         await browser.close();
+        return NextResponse.json({ "test": "hi2" }, { status: 204 })
         return image;
     } catch (error) {
         return NextResponse.json({ "error": "Couldn't fetch screenshot of website 1" }, { status: 500 })
