59b495986dc4fabf2d873c46f4d2180b781e7409
Leart Zuka <leart.zuka@web.de>
2024-06-19 02:17:11 +0200

    feat: add extra step before build

--- src/app/api/previewImage/route.ts
+++ src/app/api/previewImage/route.ts
@@ -2,24 +2,30 @@ import { chromium } from "playwright";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-    try {
-        const body = await req.json();
-        const image = await getImageBase64(body.url);
-        return NextResponse.json({ 'image': image, 'test': 'this is a test' }, { status: 200 })
-    } catch (error) {
-        return NextResponse.json({ "error": "Couldn't fetch screenshot off of website 2" }, { status: 500 })
-    }
+  try {
+    const body = await req.json();
+    const image = await getImageBase64(body.url);
+    return NextResponse.json(
+      { image: image, test: "this is a test" },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Couldn't fetch screenshot off of website 2" },
+      { status: 500 },
+    );
+  }
 }
 
 let getImageBase64 = async (url: string) => {
-    try {
-        let browser = await chromium.launch();
-        let page = await browser.newPage();
-        await page.goto(url);
-        const image = await page.screenshot();
-        await browser.close();
-        return image.toString('base64')
-    } catch (error) {
-        return `${error}`
-    }
+  try {
+    let browser = await chromium.launch();
+    let page = await browser.newPage();
+    await page.goto(url);
+    const image = await page.screenshot();
+    await browser.close();
+    return image.toString("base64");
+  } catch (error) {
+    return `${error}`;
+  }
 };
