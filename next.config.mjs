223cfed0c90f94f56fae6d2234c98fb3e6a776d3
Leart Zuka <leart.zuka@web.de>
2024-06-20 07:56:52 +0200

    chore: remove playwright install

--- next.config.mjs
+++ next.config.mjs
@@ -5,22 +5,22 @@ import rehypeKatex from "rehype-katex";
 
 const withNextIntl = createNextIntlPlugin();
 const withMDX = createMDX({
-    extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [remarkMath],
-        rehypePlugins: [rehypeKatex],
-    },
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-    experimental: {
-        serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"]
-    }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  experimental: {
+    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+  },
 };
 
 const combinedConfig = withNextIntl(withMDX(nextConfig));
--- src/app/api/previewImage/route.ts
+++ src/app/api/previewImage/route.ts
@@ -4,40 +4,40 @@ import chromium from "@sparticuz/chromium";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-    try {
-        const body = await req.json();
-        const image = await getImageBase64(body.url);
-        return NextResponse.json(
-            { image: image, test: "this is a test" },
-            { status: 200 },
-        );
-    } catch (error) {
-        return NextResponse.json(
-            { error: "Couldn't fetch screenshot off of website 2" },
-            { status: 500 },
-        );
-    }
+  try {
+    const body = await req.json();
+    const image = await getImageBase64(body.url);
+    return NextResponse.json(
+      { image: image, test: "this is a test" },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Couldn't fetch screenshot off of website 2" },
+      { status: 500 },
+    );
+  }
 }
 
 let getImageBase64 = async (url: string) => {
-    try {
-        let browser: Browser | CoreBrowser;
-        if (process.env.NODE_ENV == "production") {
-            const puppeteer = await import("puppeteer-core")
-            browser = await puppeteer.launch({
-                args: chromium.args,
-                executablePath: await chromium.executablePath(),
-            })
-        } else {
-            const puppeteer = await import("puppeteer");
-            browser = await puppeteer.launch()
-        }
-        let page = await browser.newPage();
-        await page.goto(url);
-        const image = await page.screenshot({ encoding: "base64" });
-        await browser.close();
-        return image
-    } catch (error) {
-        return `${error}`;
+  try {
+    let browser: Browser | CoreBrowser;
+    if (process.env.NODE_ENV == "production") {
+      const puppeteer = await import("puppeteer-core");
+      browser = await puppeteer.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath(),
+      });
+    } else {
+      const puppeteer = await import("puppeteer");
+      browser = await puppeteer.launch();
     }
+    let page = await browser.newPage();
+    await page.goto(url);
+    const image = await page.screenshot({ encoding: "base64" });
+    await browser.close();
+    return image;
+  } catch (error) {
+    return `${error}`;
+  }
 };
