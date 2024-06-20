021abc4792e9b56861bedb1d87d464207e8f34ad
Leart Zuka <leart.zuka@web.de>
2024-06-20 07:52:41 +0200

    try: add smaller version of chromium to be used on vercel deployment

--- src/app/api/previewImage/route.ts
+++ src/app/api/previewImage/route.ts
@@ -1,31 +1,43 @@
-import { chromium } from "playwright";
+import { Browser } from "puppeteer";
+import { Browser as CoreBrowser } from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  try {
-    const body = await req.json();
-    const image = await getImageBase64(body.url);
-    return NextResponse.json(
-      { image: image, test: "this is a test" },
-      { status: 200 },
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Couldn't fetch screenshot off of website 2" },
-      { status: 500 },
-    );
-  }
+    try {
+        const body = await req.json();
+        const image = await getImageBase64(body.url);
+        return NextResponse.json(
+            { image: image, test: "this is a test" },
+            { status: 200 },
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Couldn't fetch screenshot off of website 2" },
+            { status: 500 },
+        );
+    }
 }
 
 let getImageBase64 = async (url: string) => {
-  try {
-    let browser = await chromium.launch();
-    let page = await browser.newPage();
-    await page.goto(url);
-    const image = await page.screenshot();
-    await browser.close();
-    return image.toString("base64");
-  } catch (error) {
-    return `${error}`;
-  }
+    try {
+        let browser: Browser | CoreBrowser;
+        if (process.env.NODE_ENV == "production") {
+            const puppeteer = await import("puppeteer-core")
+            browser = await puppeteer.launch({
+                args: chromium.args,
+                executablePath: await chromium.executablePath(),
+            })
+        } else {
+            const puppeteer = await import("puppeteer");
+            browser = await puppeteer.launch()
+        }
+        let page = await browser.newPage();
+        await page.goto(url);
+        const image = await page.screenshot({ encoding: "base64" });
+        await browser.close();
+        return image
+    } catch (error) {
+        return `${error}`;
+    }
 };
