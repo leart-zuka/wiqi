11265805a40483b871510a8b4cb9e70aadf4024e
Leart Zuka <leart.zuka@web.de>
2024-09-14 02:13:26 +0200

    feat: utils for markdown file fetching

---
+++ src/app/components/utils.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+export function getMDXFiles(dir: string) {
+  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
+}
+
+export function readMDXFile(filePath: fs.PathOrFileDescriptor) {
+  let rawContent = fs.readFileSync(filePath, "utf8");
+  return matter(rawContent);
+}
+
+export function getMDXData(dir: string) {
+  let mdxFiles = getMDXFiles(dir); // file names of all posts
+
+  return mdxFiles.map((file) => {
+    let { data: metadata, content } = readMDXFile(path.join(dir, file));
+    let slug = path.basename(file, path.extname(file));
+
+    return {
+      metadata,
+      slug,
+      content,
+    };
+  });
+}
+
+export function getPostContent(folder: string, slug: string) {
+  const file = path.join(
+    process.cwd(),
+    "public",
+    "posts",
+    folder,
+    `${slug}.mdx`,
+  );
+  const content = fs.readFileSync(file, "utf8");
+  const matterResult = matter(content);
+  return matterResult;
+}
+
+export function getBlogPosts(folder: string) {
+  return getMDXData(path.join(process.cwd(), "public", "posts", folder));
+}
