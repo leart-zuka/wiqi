1273aded9cbd31735998e0fab12308f0846b686a
Leart Zuka <leart.zuka@web.de>
2024-09-15 02:33:21 +0200

    feat: automatically detect locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware({
   locales: ["en", "de"],
   defaultLocale: "de",
+  localeDetection: true,
 });
 
 export const config = {
