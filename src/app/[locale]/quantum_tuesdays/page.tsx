848e37e689c0d345888b943da85764797a7843c0
Leart Zuka <leart.zuka@web.de>
2024-09-15 20:22:24 +0200

    test: printing out messages from different static message blocks

--- src/app/[locale]/quantum_tuesdays/page.tsx
+++ src/app/[locale]/quantum_tuesdays/page.tsx
@@ -1,8 +1,15 @@
 import PostPreview from "@/app/components/PostPreview";
 import { getBlogPosts } from "@/app/components/utils";
+import { useTranslations } from "next-intl";
+
 export default function Page({ params }: { params: { locale: string } }) {
   let mdxfiles = getBlogPosts("quantum_tuesdays");
-
+  const t = useTranslations("MarkDownFiles");
+  const keys = ["test", "test1"] as const;
+  keys.map((key: string) => {
+    console.debug(`${t(`posts.${key}.title`)}`);
+  });
+  // ${t(`${key}.subtitle`)}, ${t(`${key}.date`)}
   return (
     <div className="absolute left-24 top-52">
       {mdxfiles.map((file) => {
