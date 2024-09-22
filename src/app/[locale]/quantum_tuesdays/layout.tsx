26fa6df2baa819c1483b2d23c6d928065dc09cb6
Leart Zuka <leart.zuka@web.de>
2024-09-22 04:23:50 +0200

    chore: remove stupid header from layout tsx

--- src/app/[locale]/quantum_tuesdays/layout.tsx
+++ src/app/[locale]/quantum_tuesdays/layout.tsx
@@ -1,19 +1,9 @@
-"use client";
-import DropDown from "@/app/components/DropDownSelect";
-import Example from "@/app/components/FlyoutLink";
-import { useState } from "react";
-
 export default function QTLayout({ children }: { children: React.ReactNode }) {
-  const [state, setState] = useState("");
-  return (
-    <div>
-      <div className="flex flex-row justify-between">
-        <h1 className="text-center text-2xl font-bold underline grow my-8">
-          Quantum Tuesdays
-        </h1>
-        <Example className="right-10" />
-      </div>
-      <div className="p-20">{children}</div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="flex justify-between">
+            </div>
+            <div className="p-20">{children}</div>
+        </div>
+    );
 }
