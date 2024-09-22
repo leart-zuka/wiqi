fa58ee635752e067134c76bf479f192e4ea689c1
Leart Zuka <leart.zuka@web.de>
2024-09-22 22:56:00 +0200

    feat: new pill difficulty selector

---
+++ src/app/components/DifficultySelector.tsx
@@ -0,0 +1,51 @@
+import { useCookies } from "next-client-cookies";
+import { useState } from "react";
+
+interface DifficultySelector {
+  initialDifficulty: string | undefined;
+}
+
+export default function DifficultySelector({
+  initialDifficulty,
+}: DifficultySelector) {
+  const cookies = useCookies();
+  const [difficulty, setDifficulty] = useState(initialDifficulty);
+
+  return (
+    <div className="flex flex-row-reverse">
+      <button
+        className={`p-2 border border-black hover:bg-green-500 rounded-r-full ${
+          difficulty === "college" ? "bg-green-400" : ""
+        }`}
+        onClick={() => {
+          setDifficulty("college");
+          cookies.set("difficulty", "college");
+        }}
+      >
+        🧑‍🏫
+      </button>
+      <button
+        className={`p-2 border border-y-black hover:bg-yellow-500 ${
+          difficulty === "highschool" ? "bg-yellow-400" : ""
+        }`}
+        onClick={() => {
+          setDifficulty("highschool");
+          cookies.set("difficulty", "highschool");
+        }}
+      >
+        🧑‍💻
+      </button>
+      <button
+        className={`p-2 border border-black hover:bg-red-400 rounded-l-full ${
+          difficulty === "elementary" ? "bg-red-400" : ""
+        }`}
+        onClick={() => {
+          setDifficulty("elementary");
+          cookies.set("difficulty", "elementary");
+        }}
+      >
+        🧒
+      </button>
+    </div>
+  );
+}
