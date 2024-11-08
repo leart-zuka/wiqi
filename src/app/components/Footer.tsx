19714b63fa66020a61a0ef3fe562b87f7ae9de1b
Leart Zuka <leart.zuka@web.de>
2024-11-08 23:31:55 +0100

    feat: change footer message

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-slate-700 px-3 py-3">
       <p className="text-xs text-white">
-        © 2024, Built with ❤️ by PushQuantum for Humans
+        © 2024, Built with ❤️ by Humans for Humans
       </p>
     </footer>
   );
