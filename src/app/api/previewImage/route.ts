902b0a76076cc7def78cd95c8c98fb4b45f3f3c4
Leart Zuka <leart.zuka@web.de>
2024-11-08 23:31:30 +0100

    chore: update previewImage route

--- src/app/api/previewImage/route.ts
+++ src/app/api/previewImage/route.ts
@@ -5,14 +5,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const maxDuration = 60;
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const image = await getImageBase64(body.url);
     return NextResponse.json({ image: image }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      { error: "Couldn't fetch screenshot off of website 2" },
+      { error: "Couldn't fetch screenshot off of website" },
       { status: 500 },
     );
   }
