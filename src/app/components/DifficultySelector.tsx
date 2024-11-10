0dee473df5f691dbbfbca39af84a29ccdeebd8b2
Leart Zuka <leart.zuka@web.de>
2024-11-11 00:02:25 +0100

    chore: refactoring of imports and variable names

--- src/app/components/DifficultySelector.tsx
+++ src/app/components/DifficultySelector.tsx
@@ -1,49 +1,45 @@
-import { useCookies } from "next-client-cookies";
-import { useState } from "react";
+import { setCookie } from "cookies-next/client";
 
 interface DifficultySelector {
-  initialDifficulty: string | undefined;
+  initialDifficulty: string;
+  setDifficulty: any;
 }
 
 export default function DifficultySelector({
   initialDifficulty,
+  setDifficulty,
 }: DifficultySelector) {
-  const cookies = useCookies();
-  const [difficulty, setDifficulty] = useState(
-    initialDifficulty ?? cookies.set("difficulty", "highschool"),
-  );
-
   return (
     <div className="flex flex-row-reverse">
       <button
         className={`p-2 border border-black hover:bg-red-500 rounded-r-full ${
-          difficulty === "college" ? "bg-red-400" : ""
+          initialDifficulty === "college" ? "bg-red-400" : ""
         }`}
         onClick={() => {
           setDifficulty("college");
-          cookies.set("difficulty", "college");
+          setCookie("difficulty", "college");
         }}
       >
         🧑‍🏫
       </button>
       <button
         className={`p-2 border border-y-black hover:bg-yellow-500 ${
-          difficulty === "highschool" ? "bg-yellow-400" : ""
+          initialDifficulty === "highschool" ? "bg-yellow-400" : ""
         }`}
         onClick={() => {
           setDifficulty("highschool");
-          cookies.set("difficulty", "highschool");
+          setCookie("difficulty", "highschool");
         }}
       >
         🧑‍💻
       </button>
       <button
         className={`p-2 border border-black hover:bg-green-400 rounded-l-full ${
-          difficulty === "elementary" ? "bg-green-400" : ""
+          initialDifficulty === "elementary" ? "bg-green-400" : ""
         }`}
         onClick={() => {
           setDifficulty("elementary");
-          cookies.set("difficulty", "elementary");
+          setCookie("difficulty", "elementary");
         }}
       >
         🧒
