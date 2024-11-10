fc329f139dcfb21ca1468094d0c74a5cb18af8f7
Leart Zuka <leart.zuka@web.de>
2024-11-11 00:01:36 +0100

    feat: include difficulty selector in page of quantum_tuesdays route which makes state handling a lot easier and allows for usage of useState to dynamically update links to mdx pages\

--- src/app/[locale]/quantum_tuesdays/page.tsx
+++ src/app/[locale]/quantum_tuesdays/page.tsx
@@ -1,24 +1,55 @@
+"use client";
 import PostPreview from "@/app/components/PostPreview";
-import { getBlogPosts } from "@/app/components/utils";
-import { cookies } from "next/headers";
+import { useEffect, useState } from "react";
+import DifficultySelector from "@/app/components/DifficultySelector";
+import { useCookies } from "next-client-cookies"
 
 export default function Page({ params }: { params: { locale: string } }) {
-  const cookieStore = cookies();
-  const difficulty = cookieStore.get("difficulty")?.value ?? "highschool";
-  let mdxfiles = getBlogPosts("quantum_tuesdays", params.locale, difficulty);
-  return (
-    <div className="p-2">
-      {mdxfiles.map((file) => {
-        return (
-          <PostPreview
-            key={file.slug}
-            slug={file.slug}
-            subtitle={file.metadata.subtitle}
-            date={file.metadata.date}
-            locale={params.locale}
-          />
-        );
-      })}
-    </div>
-  );
+    const cookies = useCookies();
+    const initialDifficulty = cookies.get("difficulty") ?? "highschool"
+    const [difficulty, setDifficulty] = useState(initialDifficulty);
+    const [files, setFiles] = useState([]);
+
+    let getFiles = async (difficulty: string, locale: string) => {
+        try {
+            const response = await fetch("/api/getBlogPosts", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    language: locale,
+                    difficulty: difficulty,
+                    folder: "quantum_tuesdays",
+                }),
+            });
+            const data = await response.json();
+            setFiles(data.files)
+        } catch (err) {
+            console.debug(err);
+        }
+    };
+
+    useEffect(() => {
+        getFiles(difficulty, params.locale)
+    }, [difficulty, params.locale])
+
+    return (
+        <div className="m-10">
+            <DifficultySelector initialDifficulty={difficulty} setDifficulty={setDifficulty} />
+            <div className="p-40">
+                {files.map((file) => {
+                    return (
+                        <PostPreview
+                            key={file.slug}
+                            slug={file.slug}
+                            subtitle={file.metadata.subtitle}
+                            date={file.metadata.date}
+                            locale={params.locale}
+                        />
+                    )
+                })}
+            </div>
+        </div>
+    );
 }
