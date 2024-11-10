b420eb6121e312aec6a5454743b0e65ab242d018
Leart Zuka <leart.zuka@web.de>
2024-11-10 03:06:42 +0100

    feat: update getBlogPosts api route to fetch files

--- src/app/api/getBlogPosts/route.ts
+++ src/app/api/getBlogPosts/route.ts
@@ -2,31 +2,34 @@ import { NextRequest, NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import { error } from "console";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     console.debug(body.language, body.difficulty);
+    const mdxFiles = getBlogPosts(body.folder, body.language, body.difficulty);
+    mdxFiles.map((file) => {
+      console.debug(file.slug);
+    });
+    return NextResponse.json({ files: mdxFiles });
   } catch (error) {
     return NextResponse.json(
       { error: "Couldn't retrieve blog posts" },
       { status: 500 },
     );
   }
-  return NextResponse.json({ test: "hello" }, { status: 200 });
 }
 
-export function getMDXFiles(dir: string) {
+function getMDXFiles(dir: string) {
   return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
 }
 
-export function readMDXFile(filePath: fs.PathOrFileDescriptor) {
+function readMDXFile(filePath: fs.PathOrFileDescriptor) {
   let rawContent = fs.readFileSync(filePath, "utf8");
   return matter(rawContent);
 }
 
-export function getMDXData(dir: string) {
+function getMDXData(dir: string) {
   let mdxFiles = getMDXFiles(dir); // file names of all posts
 
   return mdxFiles.map((file) => {
@@ -41,7 +44,7 @@ export function getMDXData(dir: string) {
   });
 }
 
-export function getPostContent(folder: string, slug: string) {
+function getPostContent(folder: string, slug: string) {
   const file = path.join(
     process.cwd(),
     "public",
@@ -54,11 +57,7 @@ export function getPostContent(folder: string, slug: string) {
   return matterResult;
 }
 
-export function getBlogPosts(
-  folder: string,
-  locale: string,
-  difficulty: string,
-) {
+function getBlogPosts(folder: string, locale: string, difficulty: string) {
   return getMDXData(
     path.join(process.cwd(), "public", "posts", locale, difficulty, folder),
   );
