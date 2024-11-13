c54f09221f0161ae746f87f7fef88f27f429f42e
Adatepe <adatepe.alperen.lmu@gmail.com>
2024-11-13 20:18:48 +0100

    refactor: enhance getPostContent function with error handling and slug decoding

--- src/app/components/utils.ts
+++ src/app/components/utils.ts
@@ -3,14 +3,27 @@ import path from "path";
 import matter from "gray-matter";
 
 export function getPostContent(folder: string, slug: string) {
-  const file = path.join(
-    process.cwd(),
-    "public",
-    "posts",
-    folder,
-    `${slug}.mdx`,
-  );
-  const content = fs.readFileSync(file, "utf8");
-  const matterResult = matter(content);
-  return matterResult;
+  try {
+    const decodedSlug = decodeURIComponent(slug);
+    const file = path.join(
+      process.cwd(),
+      "public",
+      "posts",
+      folder,
+      `${decodedSlug}.mdx`,
+    );
+
+    if (!fs.existsSync(file)) {
+      throw new Error(
+        `Die Datei ${decodedSlug}.mdx existiert nicht im Ordner ${folder}.`,
+      );
+    }
+
+    const content = fs.readFileSync(file, "utf8");
+    const matterResult = matter(content);
+    return matterResult;
+  } catch (error) {
+    console.error("Fehler beim Lesen des Beitragsinhalts:", error);
+    throw error;
+  }
 }
