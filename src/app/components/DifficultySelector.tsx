6244733dbb6ac14b570a55cff45e5be5a1cfee8a
Leart Zuka <leart.zuka@web.de>
2024-11-20 13:17:00 +0100

    chore:formatting

--- src/app/components/DifficultySelector.tsx
+++ src/app/components/DifficultySelector.tsx
@@ -1,63 +1,63 @@
 import { setCookie } from "cookies-next";
 
 interface DifficultySelector {
-    initialDifficulty: string;
-    setDifficulty: (difficulty: string) => void;
+  initialDifficulty: string;
+  setDifficulty: (difficulty: string) => void;
 }
 
 export default function DifficultySelector({
-    initialDifficulty,
-    setDifficulty,
+  initialDifficulty,
+  setDifficulty,
 }: DifficultySelector) {
-    const difficulties = [
-        {
-            id: "elementary",
-            emoji: "🧑‍🏫",
-            bg: "bg-green-400",
-            className:
-                "relative z-10 flex-1 rounded-full text-center font-bold text-white transition-colors duration-500 hover:bg-green-500",
-        },
-        {
-            id: "highschool",
-            emoji: "🧑‍🎓",
-            bg: "bg-yellow-400",
-            className:
-                "relative z-10 flex-1 rounded-full text-center font-bold text-white transition-colors duration-500 hover:bg-yellow-500",
-        },
-        {
-            id: "college",
-            emoji: "🧑‍🔬",
-            bg: "bg-red-400",
-            className:
-                "relative z-10 flex-1 rounded-full text-center font-bold text-white transition-colors duration-500 hover:bg-red-500",
-        },
-    ];
+  const difficulties = [
+    {
+      id: "elementary",
+      emoji: "🧑‍🏫",
+      bg: "bg-green-400",
+      className:
+        "relative z-10 flex-1 rounded-full text-center font-bold text-white transition-colors duration-500 hover:bg-green-500",
+    },
+    {
+      id: "highschool",
+      emoji: "🧑‍🎓",
+      bg: "bg-yellow-400",
+      className:
+        "relative z-10 flex-1 rounded-full text-center font-bold text-white transition-colors duration-500 hover:bg-yellow-500",
+    },
+    {
+      id: "college",
+      emoji: "🧑‍🔬",
+      bg: "bg-red-400",
+      className:
+        "relative z-10 flex-1 rounded-full text-center font-bold text-white transition-colors duration-500 hover:bg-red-500",
+    },
+  ];
 
-    const initialIndex = difficulties.findIndex(
-        (diff) => diff.id === initialDifficulty,
-    );
+  const initialIndex = difficulties.findIndex(
+    (diff) => diff.id === initialDifficulty,
+  );
 
-    return (
-        <div className="relative flex h-12 w-48 overflow-hidden rounded-full bg-slate-200">
-            <div
-                className={`absolute left-0 top-0 h-full w-1/3 rounded-full transition-all duration-500 ${initialIndex !== -1 ? difficulties[initialIndex].bg : "bg-green-400"}`}
-                style={{
-                    transform: `translateX(${initialIndex * 100}%)`,
-                }}
-            ></div>
+  return (
+    <div className="relative flex h-12 w-48 overflow-hidden rounded-full bg-slate-200">
+      <div
+        className={`absolute left-0 top-0 h-full w-1/3 rounded-full transition-all duration-500 ${initialIndex !== -1 ? difficulties[initialIndex].bg : "bg-green-400"}`}
+        style={{
+          transform: `translateX(${initialIndex * 100}%)`,
+        }}
+      ></div>
 
-            {difficulties.map((difficulty) => (
-                <button
-                    key={difficulty.id}
-                    className={difficulty.className}
-                    onClick={() => {
-                        setDifficulty(difficulty.id);
-                        setCookie("difficulty", difficulty.id);
-                    }}
-                >
-                    {difficulty.emoji}
-                </button>
-            ))}
-        </div>
-    );
+      {difficulties.map((difficulty) => (
+        <button
+          key={difficulty.id}
+          className={difficulty.className}
+          onClick={() => {
+            setDifficulty(difficulty.id);
+            setCookie("difficulty", difficulty.id);
+          }}
+        >
+          {difficulty.emoji}
+        </button>
+      ))}
+    </div>
+  );
 }
