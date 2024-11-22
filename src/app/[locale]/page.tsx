3cc4161ee59f7df687d8dd7055ffdc3651046bdf
Leart Zuka <leart.zuka@web.de>
2024-11-22 20:07:39 +0100

    chore: change hero page at least a lil bit

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,34 +1,31 @@
 "use client";
+import Image from "next/image";
 import CustomLink from "../components/CustomLink";
 import SuperpositionButton from "../components/SuperpositionButton";
 
 export default function Home() {
   return (
-    <div className="top-24 h-fit text-center">
-      <h1 className="text-5xl"> Hi this is a text </h1>
-      <div>
-        <CustomLink href="https://www.pushquantum.tech/">test</CustomLink>
-        <p>blablaba</p>
+    <div className="y-10 flex items-center justify-between px-10 lg:py-0 xl:rounded-xl xl:border-hidden">
+      <div className="space-y-5 p-10 lg:py-6">
+        <h1 className="w-11/12 max-w-xl font-serif text-6xl sm:w-9/12 md:text-7xl">
+          Welcome to the{" "}
+          <span className="bg-gradient-to-r from-blue-700 to-rose-700 bg-clip-text text-6xl font-bold text-transparent">
+            Wiqi
+          </span>{" "}
+        </h1>
+        <h1 className="w-9/12 text-3xl">
+          We want to teach the world about world of quantum. <br />
+        </h1>
+        <SuperpositionButton>
+          <p className="text-white"> Visit our current blogposts </p>
+        </SuperpositionButton>
       </div>
-      <SuperpositionButton>
-        This is a superposition button, hover above me and see what happens
-      </SuperpositionButton>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
-      <h1> Hi this is a text </h1>
+      <Image
+        src="/pq_logo.svg"
+        alt="PushQuantum Logo"
+        width={400}
+        height={400}
+      />
     </div>
   );
 }
-
-const main_colors = {
-  dark_blue: "#06014a",
-  pink: "#fe2a77",
-};
