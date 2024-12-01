dd952efc702cf756b1b2f5065a64a8b32521e1af
Adatepe, Alperen <121226886+noluyorAbi@users.noreply.github.com>
2024-12-01 02:57:04 +0100

    feat: remove mobile navigation menu and add star rating section with reviews

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -29,61 +29,6 @@ const Home: React.FC<HomeProps> = ({ params }) => {
 
   return (
     <div className="">
-      <div className="flex items-center justify-between">
-        <div className="shrink-0"></div>
-
-        {/* Mobile Navigation Menu */}
-        {isExpanded && (
-          <nav>
-            <div className="flex flex-col space-y-6 pb-4 pt-8">
-              <a
-                href="#"
-                title="Products"
-                className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"
-              >
-                Products
-              </a>
-
-              <a
-                href="#"
-                title="Features"
-                className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"
-              >
-                Features
-              </a>
-
-              <a
-                href="#"
-                title="Pricing"
-                className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"
-              >
-                Pricing
-              </a>
-
-              <a
-                href="#"
-                title="Support"
-                className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"
-              >
-                Support
-              </a>
-
-              <div className="group relative inline-flex items-center justify-center">
-                <div className="absolute -inset-px rounded-full bg-gradient-to-r from-cyan-500 to-purple-500 transition-all duration-200 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
-                <a
-                  href="#"
-                  title="Start Free Trial"
-                  className="relative inline-flex w-full items-center justify-center rounded-full border border-transparent bg-black px-6 py-2 text-base font-normal text-white"
-                  role="button"
-                >
-                  Start free trial
-                </a>
-              </div>
-            </div>
-          </nav>
-        )}
-      </div>
-
       <section className="relative min-h-screen overflow-hidden bg-black py-12 sm:pb-16 lg:pb-20 xl:pb-24">
         <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 items-center gap-x-16 gap-y-12 lg:grid-cols-2">
@@ -97,6 +42,48 @@ const Home: React.FC<HomeProps> = ({ params }) => {
               <p className="mt-4 text-lg font-normal text-gray-400 sm:mt-8">
                 {t("sub hello")}
               </p>
+
+              {/* Stars Section */}
+              <div className="mt-3 flex items-center">
+                <div className="flex">
+                  {[...Array(5)].map((_, index) => (
+                    <svg
+                      key={index}
+                      className="h-5 w-5"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M10.8586 4.71248C11.2178 3.60691 12.7819 3.60691 13.1412 4.71248L14.4246 8.66264C14.5853 9.15706 15.046 9.49182 15.5659 9.49182H19.7193C20.8818 9.49182 21.3651 10.9794 20.4247 11.6626L17.0645 14.104C16.6439 14.4095 16.4679 14.9512 16.6286 15.4456L17.912 19.3958C18.2713 20.5013 17.0059 21.4207 16.0654 20.7374L12.7052 18.2961C12.2846 17.9905 11.7151 17.9905 11.2945 18.2961L7.93434 20.7374C6.99388 21.4207 5.72851 20.5013 6.08773 19.3958L7.37121 15.4456C7.53186 14.9512 7.35587 14.4095 6.93529 14.104L3.57508 11.6626C2.63463 10.9794 3.11796 9.49182 4.28043 9.49182H8.43387C8.95374 9.49182 9.41448 9.15706 9.57513 8.66264L10.8586 4.71248Z"
+                        fill="url(#defaultGradient)"
+                      />
+                      <defs>
+                        <linearGradient
+                          id="defaultGradient"
+                          x1="3.07813"
+                          y1="3.8833"
+                          x2="23.0483"
+                          y2="6.90161"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop offset="0%" style={{ stopColor: "purple" }} />
+                          <stop
+                            offset="100%"
+                            style={{ stopColor: "#FE2B77" }}
+                          />
+                        </linearGradient>
+                      </defs>
+                    </svg>
+                  ))}
+                </div>
+                <span className="ml-2 text-base font-normal text-white">
+                  5/5
+                </span>
+                <span className="ml-1 text-base font-normal text-gray-500">
+                  2 Reviews (Leart & Alpi)
+                </span>
+              </div>
             </div>
 
             <div
