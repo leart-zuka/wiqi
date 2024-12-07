d13dd852c5631da91caf2c8443e0a19841f6d25a
Adatepe, Alperen <121226886+noluyorAbi@users.noreply.github.com>
2024-12-07 20:29:05 +0100

    feat: remove Catalyst link from Header component and enhance button styling with Flexbox properties

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -67,39 +67,7 @@ const Header = (props: HeaderProps) => {
                 Clear
               </button>
             </form>
-            <a
-              href="https://tailwindcss.com/blog/2024-05-24-catalyst-application-layouts"
-              className="ml-3 hidden items-center rounded-full bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-600 hover:bg-sky-400/20 xl:flex dark:text-sky-400"
-            >
-              <strong className="font-semibold">Introducing Catalyst</strong>
-              <svg
-                width="2"
-                height="2"
-                fill="currentColor"
-                aria-hidden="true"
-                className="ml-2 text-sky-600 dark:text-sky-400/70"
-              >
-                <circle cx="1" cy="1" r="1"></circle>
-              </svg>
-              <span className="ml-2">
-                A modern application UI kit for React
-              </span>
-              <svg
-                width="3"
-                height="6"
-                className="ml-3 overflow-visible text-sky-300 dark:text-sky-400"
-                aria-hidden="true"
-              >
-                <path
-                  d="M0 0L3 3L0 6"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-              </svg>
-            </a>
+
             <div className="relative ml-auto hidden items-center lg:flex">
               <nav className="text-sm font-semibold leading-6 text-slate-700 dark:text-slate-200">
                 <ul className="flex space-x-8">
