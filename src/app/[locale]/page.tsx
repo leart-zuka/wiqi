8802d5262a39a78df84f6b253bafd6e6917f763a
Adatepe, Alperen <121226886+noluyorAbi@users.noreply.github.com>
2024-12-07 17:56:53 +0100

    feat: upgrade react-icons to v5.4.0, update layout with bg-black, and refactor Header component for better UI

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,109 +6,108 @@ import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 interface HomeProps {
-  params: {
-    locale: string;
-  };
+    params: {
+        locale: string;
+    };
 }
 
 const Home: React.FC<HomeProps> = ({ params }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const t = useTranslations("Index");
+    const [isExpanded, setIsExpanded] = useState(false);
+    const t = useTranslations("Index");
 
-  const images = ["superpos.svg", "|1>.svg", "|0>.svg", "main.svg"];
-  const [visibleIndex, setVisibleIndex] = useState<number>(0);
+    const images = ["superpos.svg", "|1>.svg", "|0>.svg", "main.svg"];
+    const [visibleIndex, setVisibleIndex] = useState<number>(0);
 
-  const handleMouseEnter = () => {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    setVisibleIndex(randomIndex);
-  };
+    const handleMouseEnter = () => {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        setVisibleIndex(randomIndex);
+    };
 
-  const handleMouseLeave = () => {
-    setVisibleIndex(0); // Reset to default image
-  };
+    const handleMouseLeave = () => {
+        setVisibleIndex(0); // Reset to default image
+    };
 
-  return (
-    <div className="">
-      <section className="relative min-h-screen overflow-hidden bg-black py-12 sm:pb-16 lg:pb-20 xl:pb-24">
-        <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 items-center gap-x-16 gap-y-12 lg:grid-cols-2">
-            <div>
-              <h1 className="text-4xl font-normal text-white sm:text-5xl lg:text-6xl xl:text-7xl">
-                {t("hello")}{" "}
-                <span className="animate-gradient-x bg-gradient-to-r from-blue-700 to-rose-700 bg-200% bg-clip-text text-6xl font-semibold text-transparent">
-                  PushQuantum WiQi
-                </span>
-              </h1>
-              <p className="mt-4 text-lg font-normal text-gray-400 sm:mt-8">
-                {t("sub hello")}
-              </p>
+    return (
+        <div className="">
+            <section className="relative min-h-screen overflow-hidden bg-black py-12 sm:pb-16 lg:pb-20 xl:pb-24">
+                <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <div className="grid grid-cols-1 items-center gap-x-16 gap-y-12 lg:grid-cols-2">
+                        <div>
+                            <h1 className="text-4xl font-normal text-white sm:text-5xl lg:text-6xl xl:text-7xl">
+                                {t("hello")}{" "}
+                                <span className="animate-gradient-x bg-200% bg-gradient-to-r from-blue-700 to-rose-700 bg-clip-text text-6xl font-semibold text-transparent">
+                                    PushQuantum WiQi
+                                </span>
+                            </h1>
+                            <p className="mt-4 text-lg font-normal text-gray-400 sm:mt-8">
+                                {t("sub hello")}
+                            </p>
 
-              {/* Stars Section */}
-              <div className="mt-3 flex items-center">
-                <div className="flex">
-                  {[...Array(5)].map((_, index) => (
-                    <svg
-                      key={index}
-                      className="h-5 w-5"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M10.8586 4.71248C11.2178 3.60691 12.7819 3.60691 13.1412 4.71248L14.4246 8.66264C14.5853 9.15706 15.046 9.49182 15.5659 9.49182H19.7193C20.8818 9.49182 21.3651 10.9794 20.4247 11.6626L17.0645 14.104C16.6439 14.4095 16.4679 14.9512 16.6286 15.4456L17.912 19.3958C18.2713 20.5013 17.0059 21.4207 16.0654 20.7374L12.7052 18.2961C12.2846 17.9905 11.7151 17.9905 11.2945 18.2961L7.93434 20.7374C6.99388 21.4207 5.72851 20.5013 6.08773 19.3958L7.37121 15.4456C7.53186 14.9512 7.35587 14.4095 6.93529 14.104L3.57508 11.6626C2.63463 10.9794 3.11796 9.49182 4.28043 9.49182H8.43387C8.95374 9.49182 9.41448 9.15706 9.57513 8.66264L10.8586 4.71248Z"
-                        fill="url(#defaultGradient)"
-                      />
-                      <defs>
-                        <linearGradient
-                          id="defaultGradient"
-                          x1="3.07813"
-                          y1="3.8833"
-                          x2="23.0483"
-                          y2="6.90161"
-                          gradientUnits="userSpaceOnUse"
+                            {/* Stars Section */}
+                            <div className="mt-3 flex items-center">
+                                <div className="flex">
+                                    {[...Array(5)].map((_, index) => (
+                                        <svg
+                                            key={index}
+                                            className="h-5 w-5"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M10.8586 4.71248C11.2178 3.60691 12.7819 3.60691 13.1412 4.71248L14.4246 8.66264C14.5853 9.15706 15.046 9.49182 15.5659 9.49182H19.7193C20.8818 9.49182 21.3651 10.9794 20.4247 11.6626L17.0645 14.104C16.6439 14.4095 16.4679 14.9512 16.6286 15.4456L17.912 19.3958C18.2713 20.5013 17.0059 21.4207 16.0654 20.7374L12.7052 18.2961C12.2846 17.9905 11.7151 17.9905 11.2945 18.2961L7.93434 20.7374C6.99388 21.4207 5.72851 20.5013 6.08773 19.3958L7.37121 15.4456C7.53186 14.9512 7.35587 14.4095 6.93529 14.104L3.57508 11.6626C2.63463 10.9794 3.11796 9.49182 4.28043 9.49182H8.43387C8.95374 9.49182 9.41448 9.15706 9.57513 8.66264L10.8586 4.71248Z"
+                                                fill="url(#defaultGradient)"
+                                            />
+                                            <defs>
+                                                <linearGradient
+                                                    id="defaultGradient"
+                                                    x1="3.07813"
+                                                    y1="3.8833"
+                                                    x2="23.0483"
+                                                    y2="6.90161"
+                                                    gradientUnits="userSpaceOnUse"
+                                                >
+                                                    <stop offset="0%" style={{ stopColor: "purple" }} />
+                                                    <stop
+                                                        offset="100%"
+                                                        style={{ stopColor: "#FE2B77" }}
+                                                    />
+                                                </linearGradient>
+                                            </defs>
+                                        </svg>
+                                    ))}
+                                </div>
+                                <span className="ml-2 text-base font-normal text-white">
+                                    5/5
+                                </span>
+                                <span className="ml-1 text-base font-normal text-gray-500">
+                                    2 Reviews (Leart & Alpi)
+                                </span>
+                            </div>
+                        </div>
+
+                        <div
+                            className="relative h-[500px] w-full"
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
                         >
-                          <stop offset="0%" style={{ stopColor: "purple" }} />
-                          <stop
-                            offset="100%"
-                            style={{ stopColor: "#FE2B77" }}
-                          />
-                        </linearGradient>
-                      </defs>
-                    </svg>
-                  ))}
+                            {images.map((src, index) => (
+                                <img
+                                    key={index}
+                                    src={`/${src}`}
+                                    alt={`Image ${index}`}
+                                    className={`absolute inset-0 h-full w-full transition-all duration-[1200ms] ease-in-out ${visibleIndex === index
+                                            ? "z-10 scale-100 opacity-100 blur-0"
+                                            : "z-0 scale-90 opacity-0 blur-md"
+                                        }`}
+                                />
+                            ))}
+                        </div>
+                    </div>
                 </div>
-                <span className="ml-2 text-base font-normal text-white">
-                  5/5
-                </span>
-                <span className="ml-1 text-base font-normal text-gray-500">
-                  2 Reviews (Leart & Alpi)
-                </span>
-              </div>
-            </div>
-
-            <div
-              className="relative h-[500px] w-full"
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              {images.map((src, index) => (
-                <img
-                  key={index}
-                  src={`/${src}`}
-                  alt={`Image ${index}`}
-                  className={`absolute inset-0 h-full w-full transition-all duration-[1200ms] ease-in-out ${
-                    visibleIndex === index
-                      ? "z-10 scale-100 opacity-100 blur-0"
-                      : "z-0 scale-90 opacity-0 blur-md"
-                  }`}
-                />
-              ))}
-            </div>
-          </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default Home;
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FaBeer, FaGithub } from "react-icons/fa";
 
 interface HeaderProps {
   locale: string;
@@ -23,130 +24,240 @@ const Header = (props: HeaderProps) => {
   };
 
   return (
-    <nav className="w-full bg-slate-700">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          {/* Left Section: Logo */}
-          <div className="flex items-center">
-            <Link href={"/" + props.locale} className="flex items-center">
-              <Image
-                src="/pq_logo.svg"
-                className="h-8 w-8"
-                alt="PushQuantum Logo"
-                width={34}
-                height={34}
-              />
-              <span className="ml-2 text-lg font-semibold italic text-white">
-                PushQuantum
-              </span>
-            </Link>
-          </div>
-
-          {/* Center Section: Quantum Tuesdays (visible on medium and larger screens) */}
-          <div className="absolute left-1/2 hidden -translate-x-1/2 transform md:flex">
-            <Link
-              href={`/${props.locale}/quantum_tuesdays`}
-              className="font-bold text-white hover:text-sky-400"
+    <div className="sticky top-0 z-40 w-full flex-none rounded-b-md bg-white/60 backdrop-blur transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] dark:bg-[#334155]/60">
+      <div className="max-w-8xl mx-auto">
+        <div className="mx-4 border-b border-slate-900/10 py-4 lg:mx-0 lg:border-0 lg:px-8 dark:border-slate-300/10">
+          <div className="relative flex items-center">
+            <a
+              className="mr-3 flex w-[2.0625rem] items-center overflow-hidden transition-all duration-200 ease-in-out hover:scale-95 hover:text-pink-700 md:w-auto"
+              href="/"
             >
-              Quantum Tuesdays
-            </Link>
-          </div>
+              <span className="sr-only">WiQi home page</span>
+              <Image
+                src="/wq.png"
+                alt="Logo"
+                width={30}
+                height={30}
+                className="transition-transform duration-200 ease-in-out hover:rotate-12"
+              />
+              <span className="ml-3 select-none text-2xl font-semibold text-pink-600 transition-colors duration-200 ease-in-out">
+                WiQi
+              </span>
+            </a>
 
-          {/* Right Section: Locale Flag and Mobile Menu Button */}
-          <div className="flex items-center">
-            {/* Desktop Flag */}
-            <div className="hidden md:flex">
-              <Link href={replaceLocale(props.locale, pathName)}>
-                <Image
-                  src={`/${props.locale}.svg`}
-                  alt={`${props.locale} Flag`}
-                  width={24}
-                  height={24}
-                  className="self-center"
-                  priority
-                />
-              </Link>
-            </div>
-
-            {/* Mobile Menu Button */}
-            <div className="ml-2 md:hidden">
-              <button
-                onClick={toggleMenu}
-                type="button"
-                className="text-gray-200 hover:text-white focus:text-white focus:outline-none"
-                aria-controls="mobile-menu"
-                aria-expanded={isMenuOpen}
+            <a
+              href="https://tailwindcss.com/blog/2024-05-24-catalyst-application-layouts"
+              className="ml-3 hidden items-center rounded-full bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-600 hover:bg-sky-400/20 xl:flex dark:text-sky-400"
+            >
+              <strong className="font-semibold">Introducing Catalyst</strong>
+              <svg
+                width="2"
+                height="2"
+                fill="currentColor"
+                aria-hidden="true"
+                className="ml-2 text-sky-600 dark:text-sky-400/70"
               >
-                <span className="sr-only">Open main menu</span>
-                {isMenuOpen ? (
-                  // Close Icon
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
+                <circle cx="1" cy="1" r="1"></circle>
+              </svg>
+              <span className="ml-2">
+                A modern application UI kit for React
+              </span>
+              <svg
+                width="3"
+                height="6"
+                className="ml-3 overflow-visible text-sky-300 dark:text-sky-400"
+                aria-hidden="true"
+              >
+                <path
+                  d="M0 0L3 3L0 6"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            </a>
+            <div className="relative ml-auto hidden items-center lg:flex">
+              <nav className="text-sm font-semibold leading-6 text-slate-700 dark:text-slate-200">
+                <ul className="flex space-x-8">
+                  <li>
+                    <a
+                      className="transition-all duration-300 ease-in-out hover:scale-105 hover:text-pink-600 "
+                      href={`/${props.locale}/quantum_tuesdays`}
+                    >
+                      Quantum Tuesdays
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+              <div className="ml-6 flex items-center border-l border-slate-200 pl-6 dark:border-slate-500">
+                <label
+                  className="sr-only"
+                  for="headlessui-listbox-button-:r5:"
+                  id="headlessui-label-:r4:"
+                  data-headlessui-state=""
+                >
+                  Theme
+                </label>
+                <button
+                  type="button"
+                  id="headlessui-listbox-button-:r5:"
+                  aria-haspopup="listbox"
+                  aria-expanded="false"
+                  data-headlessui-state=""
+                  aria-labelledby="headlessui-label-:r4: headlessui-listbox-button-:r5:"
+                >
+                  <span className="dark:hidden">
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="h-6 w-6"
+                    >
+                      <path
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                        className="fill-sky-400/20 stroke-sky-500"
+                      ></path>
+                      <path
+                        d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
+                        className="stroke-sky-500"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span className="hidden dark:inline">
+                    <svg viewBox="0 0 24 24" fill="none" className="h-6 w-6">
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z"
+                        className="fill-sky-400/20"
+                      ></path>
+                      <path
+                        d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z"
+                        className="fill-sky-500"
+                      ></path>
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z"
+                        className="fill-sky-500"
+                      ></path>
+                    </svg>
+                  </span>
+                </button>
+
+                <div className="ml-4 hidden md:flex">
+                  <Link href={replaceLocale(props.locale, pathName)}>
+                    <Image
+                      src={`/${props.locale}.svg`}
+                      alt={`${props.locale} Flag`}
+                      width={24}
+                      height={24}
+                      className="self-center rounded-md transition-transform duration-200 ease-in-out hover:scale-95 hover:shadow-md"
+                      priority
                     />
-                  </svg>
-                ) : (
-                  // Hamburger Icon
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
+                  </Link>
+                </div>
+
+                {/* 
+                <a
+                  href="https://github.com/leart-zuka/wiqi"
+                  className="ml-6 block text-white hover:text-slate-500 dark:hover:text-slate-300"
+                >
+                  <span className="sr-only">WiQi on GitHub</span>
+                  <FaGithub className="text scale-150 rounded-3xl" />
+                </a>
+                 */}
+              </div>
+            </div>
+            <button
+              type="button"
+              className="-my-1 ml-auto flex h-8 w-8 items-center justify-center text-slate-500 hover:text-slate-600 lg:hidden dark:text-slate-400 dark:hover:text-slate-300"
+            >
+              <span className="sr-only">Search</span>
+              <svg
+                width="24"
+                height="24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                aria-hidden="true"
+              >
+                <path d="m19 19-3.5-3.5"></path>
+                <circle cx="11" cy="11" r="6"></circle>
+              </svg>
+            </button>
+            <div className="-my-1 ml-2 lg:hidden">
+              <button
+                type="button"
+                className="flex h-8 w-8 items-center justify-center text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
+              >
+                <span className="sr-only">Navigation</span>
+                <svg width="24" height="24" fill="none" aria-hidden="true">
+                  <path
+                    d="M12 6v.01M12 12v.01M12 18v.01M12 7a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
                     stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                )}
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
               </button>
+              {/* 
+              <div
+                hidden=""
+                style="position: fixed; top: 1px; left: 1px; width: 1px; height: 0px; padding: 0px; margin: -1px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; border-width: 0px; display: none;"
+              ></div>
+               */}
             </div>
           </div>
+        </div>
+        <div className="flex items-center border-b border-slate-900/10 p-4 lg:hidden dark:border-slate-50/[0.06]">
+          <button
+            type="button"
+            className="text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
+          >
+            <span className="sr-only">Navigation</span>
+            <svg width="24" height="24">
+              <path
+                d="M5 6h14M5 12h14M5 18h14"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+              ></path>
+            </svg>
+          </button>
+          <ol className="ml-4 flex min-w-0 whitespace-nowrap text-sm leading-6">
+            <li className="flex items-center">
+              Getting Started
+              <svg
+                width="3"
+                height="6"
+                aria-hidden="true"
+                className="mx-3 overflow-visible text-slate-400"
+              >
+                <path
+                  d="M0 0L3 3L0 6"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                ></path>
+              </svg>
+            </li>
+            <li className="truncate font-semibold text-slate-900 dark:text-slate-200">
+              Installation
+            </li>
+          </ol>
         </div>
       </div>
-
-      {/* Mobile Menu */}
-      {isMenuOpen && (
-        <div className="md:hidden" id="mobile-menu">
-          <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            <Link
-              href={`/${props.locale}/quantum_tuesdays`}
-              className="block rounded-md px-3 py-2 text-base font-medium text-white hover:text-sky-400"
-            >
-              Quantum Tuesdays
-            </Link>
-            <Link
-              href={replaceLocale(props.locale, pathName)}
-              className="flex items-center rounded-md px-3 py-2 text-base font-medium text-white hover:text-sky-400"
-            >
-              <Image
-                src={`/${props.locale}.svg`}
-                alt={`${props.locale} Flag`}
-                width={24}
-                height={24}
-                className="mr-2"
-                priority
-              />
-              {props.locale.toUpperCase()}
-            </Link>
-          </div>
-        </div>
-      )}
-    </nav>
+    </div>
   );
 };
 
