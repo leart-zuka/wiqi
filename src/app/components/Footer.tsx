ed0de085529ea22ed79adf59a2ebe22aeff9f2aa
Adatepe, Alperen <121226886+noluyorAbi@users.noreply.github.com>
2024-12-15 21:23:38 +0100

    make footer heart pulse

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,9 @@
 export default function Footer() {
   return (
     <footer className="bg-slate-700 px-3 py-3">
-      <p className="text-xs text-white">
-        © 2024, Built with ❤️ by Humans for Humans
+      <p className="select-none text-xs text-white">
+        © 2024, Built with <span className="hover:animate-pulse">❤️</span> by
+        Humans for Humans
       </p>
     </footer>
   );
